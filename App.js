97021589acb0a0a8db3364c75ecf66d478a2bf1d
Valentin <valentin.bouis@gmail.com>
2023-01-18 13:37:23 -0500

    clean

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
-// import { useState } from 'react';
 import useState from 'react-usestateref'
 import DatePicker from 'react-native-neat-date-picker'
-import DropDownPicker from 'react-native-dropdown-picker';
-
 import { SelectList } from 'react-native-dropdown-select-list'
 
 export default function App() {
@@ -17,12 +14,10 @@ export default function App() {
       {key:'student', value:'Etudiant'},
   ]
 
-
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [mode, setMode] = useState('arrivee');
   const [dateArrivee, setDateArrivee] = useState(JSON.stringify(new Date()).split('T')[0].slice(1));
   const [dateDepart, setDateDepart] = useState(JSON.stringify(new Date()).split('T')[0].slice(1));
-
 
   const openDatePicker = (mode) => {
     setMode(mode);
@@ -34,33 +29,30 @@ export default function App() {
   }
 
   const onConfirm = ( date ) => {
-    console.log('TEST DATE' + JSON.stringify(date).split('T')[0].split(':')[1].slice(1));
     setShowDatePicker(false)
     if (mode == 'depart') 
       setDateDepart(JSON.stringify(date).split('T')[0].split(':')[1].slice(1));
     if (mode == 'arrivee')
       setDateArrivee(JSON.stringify(date).split('T')[0].split(':')[1].slice(1));
-
   }
-
 
   const onSend = () => {
     console.log(dateArrivee);
     console.log(dateDepart);
     console.log(ageCategory);
 
-    // fetch('http://127.0.0.1:5000/optimize', {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     dateArrivee: dateArrivee,
-    //     dateDepart: dateDepart,
-    //     categorieAge: ageCategory
-    //   }),
-    // });
+    fetch('http://127.0.0.1:5000/optimize', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        dateArrivee: dateArrivee,
+        dateDepart: dateDepart,
+        categorieAge: ageCategory
+      }),
+    });
   }
   
 
